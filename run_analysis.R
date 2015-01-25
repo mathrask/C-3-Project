c29328e26e99d94278078417f510c2d196529533
mathrask <mathrask@users.noreply.github.com>
2015-01-25 11:16:10 -0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,6 +2,6 @@ require(knitr)
 require(markdown)
 #Set wd
 path <-getwd()
-setwd(")
+setwd("path")
 knit("run_analysis.Rmd", encoding="ISO8859-1")
 markdownToHTML("run_analysis.md", "run_analysis.html")
